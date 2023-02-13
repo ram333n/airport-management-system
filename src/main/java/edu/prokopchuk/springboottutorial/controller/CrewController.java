a8ba59c53dc0d232c35404e5b4d5f18fcf29216b
Roman Prokopchuk <ghost62727@gmail.com>
2023-02-13 15:49:48 +0200

    Add crew edit form TODO: fix issue with POST

--- src/main/java/edu/prokopchuk/springboottutorial/controller/CrewController.java
+++ src/main/java/edu/prokopchuk/springboottutorial/controller/CrewController.java
@@ -3,11 +3,15 @@ package edu.prokopchuk.springboottutorial.controller;
 import edu.prokopchuk.springboottutorial.model.CrewMember;
 import edu.prokopchuk.springboottutorial.service.CrewService;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class CrewController {
 
@@ -24,6 +28,24 @@ public class CrewController {
     model.addAttribute("crew", crew);
 
     return "crew";
+  }
+
+  @ModelAttribute(name = "crewMember")
+  public CrewMember crewMember() {
+    return new CrewMember();
+  }
+
+  @GetMapping("/crew/new")
+  public String showCreateForm() {
+    return "crew-new-form";
+  }
+
+  @PostMapping("/crew")
+  public String createCrewMember(@ModelAttribute("crewMember") CrewMember crewMember) {
+    log.info("Created: {}", crewMember);
+    crewService.createCrewMember(crewMember);
+
+    return "redirect:/crew";
   }
 
 }
