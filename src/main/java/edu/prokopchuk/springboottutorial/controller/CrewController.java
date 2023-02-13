b0bad0efaf005905e82ed6e85cbf57c0254629c1
Roman Prokopchuk <ghost62727@gmail.com>
2023-02-13 19:59:37 +0200

    Add PUT method

--- src/main/java/edu/prokopchuk/springboottutorial/controller/CrewController.java
+++ src/main/java/edu/prokopchuk/springboottutorial/controller/CrewController.java
@@ -3,13 +3,16 @@ package edu.prokopchuk.springboottutorial.controller;
 import edu.prokopchuk.springboottutorial.model.CrewMember;
 import edu.prokopchuk.springboottutorial.service.CrewService;
 import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Slf4j
 @Controller
@@ -39,8 +42,28 @@ public class CrewController {
 
   @PostMapping("/crew")
   public String createCrewMember(@ModelAttribute("crewMember") CrewMember crewMember) {
-    log.info("Created: {}", crewMember);
     crewService.createCrewMember(crewMember);
+
+    return "redirect:/crew";
+  }
+
+  @GetMapping("/crew/edit/{pass-number}")
+  public String showEditForm(@PathVariable("pass-number") String passNumber,
+                             Model model) {
+    Optional<CrewMember> crewMember = crewService.getCrewMember(passNumber);
+
+    if (crewMember.isEmpty()) {
+      throw new IllegalArgumentException("Crew member with specific id found");
+    }
+
+    model.addAttribute("crewMember", crewMember.get());
+
+    return "crew-edit-form";
+  }
+
+  @PutMapping("/crew")
+  public String editCrewMember(@ModelAttribute("crewMember") CrewMember crewMember) {
+    crewService.updateCrewMember(crewMember);
 
     return "redirect:/crew";
   }
